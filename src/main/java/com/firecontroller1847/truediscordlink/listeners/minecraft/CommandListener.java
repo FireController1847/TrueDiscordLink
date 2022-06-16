25ebe95908fc37ae3363747140fea50304e25470
Nathaniel M <nmoratto629@gmail.com>
2022-06-16 00:40:08 -0700

    Fixed Tellraw relay not parsing tags (#10)
    * Fixed Tellraw relay not parsing tags.
    * Added comma to Collection Join
    * v1.3.1
    
    Co-authored-by: FireController#1847 <usfirepilot123@gmail.com>

--- src/main/java/com/firecontroller1847/truediscordlink/listeners/minecraft/CommandListener.java
+++ src/main/java/com/firecontroller1847/truediscordlink/listeners/minecraft/CommandListener.java
@@ -2,15 +2,20 @@ package com.firecontroller1847.truediscordlink.listeners.minecraft;
 
 import com.firecontroller1847.truediscordlink.DiscordManager;
 import com.firecontroller1847.truediscordlink.TrueDiscordLink;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.SelectorComponent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.chat.ComponentSerializer;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.server.ServerCommandEvent;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CommandListener implements Listener {
 
@@ -25,7 +30,7 @@ public class CommandListener implements Listener {
     // Player Commands
     @EventHandler
     public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
-        this.onCommand(event.getMessage().substring(1), event.getPlayer());
+        this.onCommand(event.getMessage(), event.getPlayer());
     }
 
     // Non-Player Commands
@@ -36,6 +41,11 @@ public class CommandListener implements Listener {
 
     // Event
     public void onCommand(String command, CommandSender sender) {
+        // Check for starting slash
+        if (command.startsWith("/")) {
+            command = command.substring(1);
+        }
+
         // Relay Tellraw Messages
         if (discordlink.getConfig().getBoolean("events.relay_tellraw_messages") && command.startsWith("tellraw")) {
 
@@ -48,8 +58,23 @@ public class CommandListener implements Listener {
                     String message = String.join(" ", Arrays.copyOfRange(args, 2, args.length));
                     if (targets.contains("@a")) {
                         try {
-                            MutableComponent components = Component.Serializer.fromJson(message);
-                            String stringifiedTellraw = components.getString();
+                            BaseComponent[] components = ComponentSerializer.parse(message);
+                            if (message.contains("@a") || message.contains("@e") || message.contains("@r") || message.contains("@s") || message.contains("@p")) {
+                                List<Entity> entities = sender.getServer().selectEntities(sender, targets);
+                                if (!entities.isEmpty()) {
+                                    for (int i = 0; i < components.length; i++) {
+                                        BaseComponent component = components[i];
+                                        if (component instanceof SelectorComponent) {
+                                            List<Entity> entitiesInSelector = sender.getServer().selectEntities(sender, ((SelectorComponent) component).getSelector());
+                                            components[i] = new TextComponent(entitiesInSelector.stream().map(CommandSender::getName).collect(Collectors.joining(", ")));
+                                        }
+                                    }
+                                }
+                            }
+                            String stringifiedTellraw = Arrays.stream(components).map(c -> c.toPlainText()).collect(Collectors.joining());
+                            if (stringifiedTellraw.length() >= 1995) {
+                                stringifiedTellraw = stringifiedTellraw.substring(0, 1994) + "...";
+                            }
                             discordManager.sendDiscordMessage(stringifiedTellraw);
                         } catch (Exception e) {
                             // ...
@@ -57,7 +82,6 @@ public class CommandListener implements Listener {
                     }
                 }
             }
-
         }
     }
 
